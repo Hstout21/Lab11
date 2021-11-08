ca0af0a0b4d23d8e57aaea68f1f239dfdda1f273
Hstout21 <89660849+Hstout21@users.noreply.github.com>
2021-11-08 00:01:31 -0500

    Add files via upload

---
+++ Lab11.cc
@@ -0,0 +1,67 @@
+/**
+ *   @file: Lab11.cc
+ * @author: Hunter Stout
+ *   @date: 11/5/2021
+ *  @brief: Pre-defined character functions and arrays.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+//#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+//15.
+//const int SIZE = 6;
+//int graph[SIZE] = {5, 3, 2, 7, 1, 9};
+
+//for (int i = 0; i < SIZE; i++) {
+//    cout << setfill('-') << left << setw(5) << graph[i];
+//    for (int j = 0; j < graph[i]; j++) {
+//        cout << setw(2) << '*';
+//    }
+//    cout << endl;
+//}
+
+//(Declares for questions 1-14)
+//const int SIZE = 10;
+//double a_list[] = {3.2, 4.6, 7.0, 12.5, 8.3, 9.1};
+//int nums[SIZE];
+//char vowels[] = {'A', 'E', 'I', 'O', 'U'};
+//string name[SIZE];
+//float values[SIZE];
+//int i = 3;
+
+    //3.
+    //cout << sizeof(a_list) << endl;
+    //cout << sizeof(nums) << endl;
+    //cout << sizeof(vowels) << endl;
+    //cout << sizeof(name) << endl;
+    //cout << sizeof(values) << endl;
+
+    //12.
+    //for (i = 0; i < SIZE; i++) {
+    //    nums[i] = i * i;
+    //    cout << nums[i] << endl;
+    //}
+
+    //13.
+    //for (i = SIZE -1; i >= 0; i--) {
+    //    cout << nums[i] << " ";
+    //}
+    //cout << endl;
+
+    //14.
+    //i = 1;
+    //nums[0] = 1;
+    //do {
+    //    nums[i] = 2 * nums[i-1];
+    //    cout << nums[i] << endl;
+    //    i++;
+    //} while (i < SIZE);
+
+    return 0;
+}
